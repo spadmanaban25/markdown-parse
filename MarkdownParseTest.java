30ddaad49a3de46a8d2ee68b9c0866f0b46e4928
clingunis <clingunis.123@gmail.com>
2022-01-27 18:53:18 -0800

    JUnit Tester Implemented test-file.md Test

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,20 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+    @Test
+    public void testGetLinks() throws IOException{
+        String file=Files.readString(Path.of("test-file.md"));
+        assertEquals(MarkdownParse.getLinks(file), List.of("https://something.com","some-page.html"));
     }
 }
