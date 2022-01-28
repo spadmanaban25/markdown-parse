df6bca8c6a2e53f0fbd18ef0f4943f18b4e86034
spadmanaban25 <gsspadmanaban@gmail.com>
2022-01-28 10:53:37 -0500

    Update MarkdownParse.java
    Showing step by step on how to fix/improve the code (if statement)

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -13,11 +13,13 @@ public class MarkdownParse {
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
-            int openParen = markdown.indexOf("(", nextCloseBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+            if(nextOpenBracket > 0){
+                int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
+                int openParen = markdown.indexOf("(", nextCloseBracket);
+                int closeParen = markdown.indexOf(")", openParen);
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+                currentIndex = closeParen + 1;
+            }
         }
         return toReturn;
     }
