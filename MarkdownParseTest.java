af4c01a897079384af3e8448728be2a485640834
spadmanaban25 <gsspadmanaban@gmail.com>
2022-02-25 10:15:32 -0500

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -14,39 +14,10 @@ public class MarkdownParseTest {
     }
     @Test
     public void testGetLinks1() throws IOException{
-        String file=Files.readString(Path.of("test-file.md"));
+        String file=Files.readString(Path.of("test-file1.md"));
         assertEquals(MarkdownParse.getLinks(file), List.of("https://something.com","some-page.html"));
     }
-    @Test
-    public void testGetLinks2() throws IOException{
-        String file=Files.readString(Path.of("test-file2.md"));
-        assertEquals(MarkdownParse.getLinks(file), 
-        List.of("https://www.youtube.com/watch?v=dQw4w9WgXcQ","https://www.google.com/maps"));
-    }
-    @Test
-    public void testGetLinks3() throws IOException{
-        String file=Files.readString(Path.of("test-file3.md"));
-        assertEquals(MarkdownParse.getLinks(file), 
-        List.of("https://www.youtube.com/watch?v=dQw4w9WgXcQ"));
-    }
-    @Test
-    public void testGetLinksBulk() throws IOException{
-        String file=Files.readString(Path.of("test-file4.md"));
-        assertEquals(MarkdownParse.getLinks(file),List.of());
-        file=Files.readString(Path.of("test-file6.md"));
-        assertEquals(MarkdownParse.getLinks(file),List.of());
-        file=Files.readString(Path.of("test-file7.md"));
-        assertEquals(MarkdownParse.getLinks(file),List.of());
-        //tests below are bugged
-        file=Files.readString(Path.of("test-file8.md"));
-        assertEquals(MarkdownParse.getLinks(file),List.of("a link on the first line"));
-        file=Files.readString(Path.of("test-file5.md"));
-        assertEquals(MarkdownParse.getLinks(file),List.of());
-    }
-    @Test
-    public void willFail(){
-        assertEquals(1, 1);
-    }
+
     //@Test
     //public void testGetLinksImage2()throws IOException{
         //String file=Files.readString(Path.of("test-file9.md"));
